455ae3539cf9d9d49fa377fd9cc389b1944f8495
BeastArgons <226310744+dhruvsharmauni-png@users.noreply.github.com>
2025-12-03 12:19:18 +0530

    //

--- SubSetCheck.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class SubSetCheck {
-    public static void main(String[] a) {
-        Set<Integer> s1 = new HashSet<>(Arrays.asList(2, 3));
-        Set<Integer> s2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
-        System.out.println(s2.containsAll(s1));
-    }
-}
